d7225002f67fec5a34f45ed2f5f28bcad24fe674
kotk100 <kern.ziga@gmail.com>
2017-11-18 12:21:22 +0100

    Project setup, travis.yml

---
+++ src/main/java/com/rso/streaming/clip.java
@@ -0,0 +1,7 @@
+package com.rso.streaming;
+
+public class clip {
+  public static void main(String[] argv) {
+    // Please, do not remove this line from file template, here invocation of web service will be inserted  
+  }
+}
